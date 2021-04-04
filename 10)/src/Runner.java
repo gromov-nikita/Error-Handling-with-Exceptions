751c5d0a35906080b752eeb723d59d9ad5f6d431
gromov-nikita <hromay.n999@gmail.com>
2021-04-04 13:10:53 +0300

    1

--- 10)/src/Runner.java
+++ 10)/src/Runner.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 /*
 Exercise 10: (2) Create a class with two methods, f( ) and g( ). In g( ), throw an
 exception of a new type that you define. In f( ), call g( ), catch its exception and, in the catch
@@ -5,18 +7,18 @@ clause, throw a different exception (of a second type that you define). Test you
 main( ).
  */
 public class Runner {
-    public static void throwNullEx() {
+    public static void throwNullEx() throws IOException {
         nullEx();
     }
-    public static void nullEx() {
-        throw new NullPointerException("NULLLLLLLLL...");
+    public static void nullEx() throws IOException {
+        throw new IOException("NULLLLLLLLL...");
     }
 
     public static void main(String[] args) throws Exception {
         try {
             Runner.throwNullEx();
         }
-        catch(NullPointerException n) {
+        catch(IOException n) {
             throw new Exception("<<<Exceprion>>>");
         }
     }
